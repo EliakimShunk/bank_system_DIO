077130dfd3e1f6e53b7f5c40e2d7653db801a212
EliakimShunk <shunkeliakim@gmail.com>
2024-08-31 16:48:55 -0300

    added commentaries to function in bank_functions.py

--- fc/bank_functions.py
+++ fc/bank_functions.py
@@ -1,9 +1,11 @@
 def deposit(balance, deposit_value):
+    # basic adding function
     local_balance = balance
     return local_balance + deposit_value
 
 
 
 def withdraw(balance, withdraw_value):
+    # basic subtraction function
     local_balance = balance
     return local_balance - withdraw_value
