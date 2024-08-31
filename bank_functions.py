d33a2090196d710ab2804dd322ffe7fef98af085
EliakimShunk <shunkeliakim@gmail.com>
2024-08-31 15:50:40 -0300

    created functions and added them to the main program

---
+++ bank_functions.py
@@ -0,0 +1,9 @@
+def deposit(balance, deposit_value):
+    local_balance = balance
+    return local_balance + deposit_value
+
+
+
+def withdraw(balance, withdraw_value):
+    local_balance = balance
+    return local_balance - withdraw_value
